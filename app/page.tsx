b9b414938523c95c23fbe2471af07f85b6d95bef
jennncho <chojenny112@gmail.com>
2024-01-17 20:14:19 -0500

    descriptions edit

--- app/page.tsx
+++ app/page.tsx
@@ -31,11 +31,11 @@ const features = [
   {
     name: 'Commercial Cleaning',
     description:
-      'No matter the size of your organization, we offer top-tier cleaning and facilities maintenance services that meet and exceed industry standards.',
+      'We offer top-tier cleaning and facilities maintenance services for your organization that meet and exceed industry standards.',
     icon: BuildingOffice2Icon,
   },
   {
-    name: 'Specialty',
+    name: 'Adaptability',
     description:
       'We provides expert cleaning and facilities maintenance for businesses of all sizes, ensuring a cleaner, healthier, and more productive work environment.',
     icon: SparklesIcon,
@@ -47,9 +47,9 @@ const features = [
     icon: WrenchScrewdriverIcon,
   },
   {
-    name: 'Advanced pro',
+    name: 'One-Stop-Shop',
     description:
-      'Your One-Stop Shop for Expert Cleaning Services. Experience the difference with Advanced Pro, your trusted partner for sparkling clean spaces. ',
+      'Your One-Stop Shop for Expert Cleaning Services. Experience the difference with a consolidated, catch-all service for all your cleaning and maintenence needs. ',
     icon: ShieldCheckIcon,
   },
 ]
@@ -194,9 +194,9 @@ export default function Home() {
 <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
-          <h2 className="text-base font-semibold leading-7 text-indigo-600">Featured</h2>
+          <h2 className="text-base font-semibold leading-7 text-indigo-600">Ace C&M</h2>
           <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Our Highlighted Services
+            What Sets Us Apart
           </p>
         </div>
         <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
